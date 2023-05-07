f028f69fd36cb9bcf3a8c95ee3013ef5f223d603
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-07 20:23:15 +0400

    Head

--- burger/pages/404.js
+++ burger/pages/404.js
@@ -1,6 +1,14 @@
 const { default: Link } = require('next/link');
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const notFoundPage = () => {
+	const router = useRouter();
+	useEffect(() => {
+		setTimeout(() => {
+			router.push('/');
+		}, 2000);
+	}, []);
 	return (
 		<div className='not-found'>
 			<h1>Ой</h1>
--- burger/pages/_app.js
+++ burger/pages/_app.js
@@ -1,8 +1,12 @@
 import '@/styles/globals.css';
 import Layout from '@/components/layout';
+import Head from 'next/head';
 export default function App({ Component, pageProps }) {
 	return (
 		<Layout>
+			<Head>
+				<title>Бургеры</title>
+			</Head>
 			<Component {...pageProps} />
 		</Layout>
 	);
