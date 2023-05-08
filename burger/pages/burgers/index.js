4278d69b83a9e13aef236fd0ee1a96daf5274440
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-08 22:15:46 +0400

    getStaticProps

--- burger/pages/burgers/index.js
+++ burger/pages/burgers/index.js
@@ -1,7 +1,36 @@
-const AllBurgers = () => {
+import Link from 'next/link';
+import styles from '../../styles/Burgers.module.css';
+import Image from 'next/image';
+export const getStaticProps = async () => {
+	const res = await fetch('http://localhost:5000/items');
+	const data = await res.json();
+	return {
+		props: {
+			burgers: data,
+		},
+	};
+};
+
+const AllBurgers = ({ burgers }) => {
 	return (
 		<div>
 			<h1>Все бургеры</h1>
+			{burgers.map((burger) => {
+				console.log(burger);
+				return (
+					<Link href={`/burger/${burger.id}`} key={burger.id}>
+						<div className={styles.burgerCard}>
+							<div className={styles.imageContainer}>
+								<Image src={`${burger.image}`} width={100} height={100} alt='burger'></Image>
+							</div>
+							<div>
+								<h3>{burger.name}</h3>
+								<p>{burger.desc}</p>
+							</div>
+						</div>
+					</Link>
+				);
+			})}
 		</div>
 	);
 };
